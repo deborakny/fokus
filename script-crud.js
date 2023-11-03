cb24baa12d70443f0358264dd7f9e5c2d2dc7f07
Debora Kuniy <debora.n_@hotmail.com>
2023-11-03 18:17:14 -0300

    exibindo a lista de tarefas na tela

--- script-crud.js
+++ script-crud.js
@@ -12,14 +12,13 @@ let tasks = [
     }
 ];
 
-const taskIconSvg = `<svg class="app_section-task-icon-status" width="24" height="24" viewBox="0 0 24 24"
+const taskIconSvg = `<svg class="app__section-task-icon-status" width="24" height="24" viewBox="0 0 24 24"
 fill="none" xmlns="http://www.w3.org/2000/svg">
-<circle cx="12" cy="12" r="12" 
-    fill="#FFF" />
+<circle cx="12" cy="12" r="12" fill="#FFF" />
 <path
-    d "M9 16.1719119.5938 5.57812121 6.9843819 18.9844L3.42188 13.4062L4.82812 12L19 16.17192"
-    fill="#01080E" 
-/></svg>`
+    d="M9 16.1719L19.5938 5.57812L21 6.98438L9 18.9844L3.42188 13.4062L4.82812 12L9 16.1719Z"
+    fill="#01080E" />
+</svg>`
 
 function createTask(task) {
     const li = document.createElement('li');
@@ -38,3 +37,8 @@ function createTask(task) {
 
     return li;
 }
+
+tasks.forEach(task => {
+    const taskItem = createTask(task);
+    taskListContainer.appendChild(taskItem);
+})
